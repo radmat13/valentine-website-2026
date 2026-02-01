f830d41cc1a190d4a82a276080e24e473eb301f6
radmat13 <radmatovec@gmail.com>
2026-02-01 21:36:01 +0100

    config3.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dwcn11txn/video/upload/v1769977996/Stephen_Sanchez_-_Until_I_Found_You_Official_Video_lzv1vr.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
